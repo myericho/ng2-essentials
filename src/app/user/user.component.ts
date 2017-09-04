38d5fb67881dd8253a20420e9505c1583902d084
myericho <myericho.ho@gmail.com>
2017-09-05 00:31:13 +0800

    加入外部@Input 參考

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  name = 'Eric';
+  @Input() name;
   eventName = 'test';
 
   constructor() { }
